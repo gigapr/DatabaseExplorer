95d7aad0e2fbca9ec1fc0d57a2c2f60b25f520a6
Gaetano <gigapr@hotmail.com>
2012-12-06 16:44:31 +0000

    Introducing requirejs

---
+++ Src/DatabaseSchemaReader.Website/Scripts/homeModule.js
@@ -0,0 +1,92 @@
+﻿define(["jquery", "easing", "flowplayer", "validate", "validateUnobtrusive", "unobtrusiveAjax"], function ($) {
+
+    $(document).ready(function () {
+        animateBoxes();
+        addScrollingEffectToMenuLinks();
+    });
+
+    function addScrollingEffectToMenuLinks() {
+        var elements = new Array("ul.nav a", "#signInTrigger", "#registerTrigger", ".trigger");
+
+        $(elements).each(function (index, value) {
+            $(function () {
+                $(value).bind('click', function (event) {
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollLeft: $($anchor.attr('href')).offset().left
+                    }, 1500, 'easeInOutExpo');
+                    event.preventDefault();
+                });
+            });
+        });
+    }
+
+    function animateBoxes() {
+        $("#animateRedWebApi").animate({
+            opacity: 0.5,
+            left: "960px",
+            borderWidth: "10px"
+        }, 1500);
+        $("#animateGrayWebApi").animate({
+            opacity: 0.5,
+            top: "330px",
+            borderWidth: "10px"
+        }, 1500);
+        $("#animateYellowWebApi").animate({
+            opacity: 0.5,
+            left: "980px",
+            borderWidth: "10px"
+        }, 1500);
+        $("#animateBlueWebApi").animate({
+            top: "375px",
+            borderWidth: "10px"
+        }, 1500);
+
+        $("#animateBlueExplorer").animate({
+            left: "100px",
+            borderWidth: "10px"
+        }, 1500);
+
+        $("#animateBlueQueryBuilder").animate({
+            top: "375px",
+            borderWidth: "10px"
+        }, 1500);
+
+        $("#animateYellowExplorer").animate({
+            opacity: 0.5,
+            left: "30px",
+            borderWidth: "10px"
+        }, 1500);
+
+        $("#animateRedExplorer").animate({
+            opacity: 0.5,
+            top: "330px",
+            borderWidth: "10px"
+        }, 1500);
+
+        $("#animateGrayQueryBuilder").animate({
+            opacity: 0.5,
+            top: "330px",
+            borderWidth: "10px"
+        }, 1500);
+
+        $("#animateGrayExplorer").animate({
+            opacity: 0.5,
+            left: "75px",
+            borderWidth: "10px"
+        }, 1500);
+
+        $("#animateYellowQueryBuilder").animate({
+            opacity: 0.5,
+            left: "520px",
+            borderWidth: "10px"
+        }, 1500);
+
+        $("#animateRedQueryBuilder").animate({
+            opacity: 0.5,
+            left: "530px",
+            borderWidth: "10px"
+        }, 1500);
+    }
+});
+
--- Src/DatabaseSchemaReader.Website/Scripts/require.config.js
+++ Src/DatabaseSchemaReader.Website/Scripts/require.config.js
@@ -1,9 +1,26 @@
 ﻿var require = {
+    deps: ["jquery"],
     baseUrl: "scripts",
     paths: {
         "jquery": [
             "//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min",
-            "lib/jquery.min"
+            "libs/jquery1.8.3.min"
+        ],
+        "easing": [
+            "libs/jquery.easing.1.3"
+        ],
+        "flowplayer": [
+            "libs/flowplayer-3.2.6.min"
+        ],
+        "validate": [
+            "libs/jquery.validate.min"
+        ],
+        "validateUnobtrusive": [
+            "libs/jquery.validate.unobtrusive.min"
+        ],
+        "unobtrusiveAjax": [
+            "libs/jquery.unobtrusive-ajax.min"
         ]
     }
 };
+
